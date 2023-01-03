a4c280cfdc633d0fbf3f455268ff970406b6d81d
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2023-01-03 21:01:24 +0100

    pagination added, not functional

--- my-app/src/components/Artworks.jsx
+++ my-app/src/components/Artworks.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Artwork from './Artwork'
-import {CircularProgress} from '@mui/material/'
+import {Box,CircularProgress, Pagination, Stack} from '@mui/material/'
 
 const apiKey = "9fcbde6d-b1de-4546-8974-eef81e8f90f4";
 let apiAddress = `https://api.harvardartmuseums.org/object?size=100&page=1?&apikey=${apiKey}`;
@@ -19,17 +19,22 @@ function Artworks() {
 
   return (
     <div className="artworks">
-            {arts != 0 ? (
-            arts.records             
-            .map((art, index) => (
-                <Artwork key={index} artData={art} />
-            ))
-      ) : (
-        <CircularProgress />
+        {arts != 0 ? (
+          <Box>
+            {arts.records             
+              .map((art, index) => (
+              <Artwork key={index} artData={art} />
+            ))}
+              <Stack sx={{ display: 'center', justifyContent: 'center', alignItems: 'center'}} spacing={2}>
+                <Pagination count={arts.info.pages} shape="rounded" />
+              </Stack>
+          </Box>
+        ) : (
+          <Box sx={{ display: 'center', justifyContent: 'center', alignItems: 'center'}}>
+          <CircularProgress />
+          </Box>
       )}
-      
-
-</div>
+    </div>
   )
 }
 
