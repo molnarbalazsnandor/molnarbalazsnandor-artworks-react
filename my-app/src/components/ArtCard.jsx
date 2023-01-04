d9b10f775e0171d46d0e208063dc6d98630bcc1f
Timi <timeaher@gmail.com>
2023-01-04 20:21:48 +0100

    ArtCard changes

--- my-app/src/components/ArtCard.jsx
+++ my-app/src/components/ArtCard.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
-import Button from '@mui/material';
+import { Card, CardContent, Button,CardActions, Typography, CardMedia } from '@mui/material/'
+import { blue, green, orange, purple, red } from '@mui/material/colors';
 
-function ArtCard(artData) {
+function ArtCard({ artData }) 
+ {
+    return (
+ <>
+      <Card sx={{ maxWidth: 400 }}>
+        <CardMedia
+          sx={{ height: 250 }}
+          image={artData.primaryimageurl}
+          title={artData.title}
+        />
+        <CardContent>
+          <Typography sx={{ fontSize:16, color:blue }} >
+          {artData.displayname}
+          </Typography>
+          <Typography sx={{ fontSize:14, color:green }}>
+            {artData.worktype}
+          </Typography>
+          <Typography sx={{ fontSize:14, color:purple }}>
+            {artData.commentary}
+          </Typography>
+          <Typography sx={{ fontSize:14, color:red }}>
+            {artData.yearmade}
+          </Typography>
+          <Typography sx={{ fontSize:16, color:orange }}>
+            {artData.rank}
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <Button size="medium" variant="outlined" backgroundColor="blue" color="white" onClick= {() =>{}}>Return</Button>
+        </CardActions>
+      </Card>
+  </>
+/*function ArtCard(artData) {
 
     const navigate = useNavigate();
 
@@ -17,7 +49,7 @@ function ArtCard(artData) {
     <p>
     <Button variant="outlined"onClick={() => navigate("/Artwork")}>Return</Button>
     </p>
-    </div>
+    </div>*/
     
 
   )
