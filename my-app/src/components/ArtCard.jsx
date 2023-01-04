9ac906494000662cecd164e92bdab184e26a6e5f
Timi <timeaher@gmail.com>
2023-01-04 18:54:27 +0100

    change in Button

--- my-app/src/components/ArtCard.jsx
+++ my-app/src/components/ArtCard.jsx
@@ -14,7 +14,8 @@ function ArtCard(artData) {
     <h4>{artData.yearmade}</h4>
     <h4>{artData.commentary}</h4>
     <h4>{artData.rank}</h4>
-    <p><button onClick={() => navigate("/Artwork")}>Return</button>
+    <p>
+    <Button variant="outlined"onClick={() => navigate("/Artwork")}>Return</Button>
     </p>
     </div>
     
