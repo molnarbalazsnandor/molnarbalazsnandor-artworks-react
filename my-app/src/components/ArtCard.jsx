4c21116876c898aff10ceaa8696c8c2c6bc131c5
Timi <timeaher@gmail.com>
2023-01-04 11:18:47 +0100

    initial commit

---
+++ my-app/src/components/ArtCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+function ArtCard(artData) {
+
+    const navigate = useNavigate();
+
+  return (
+    <div>
+    <h2>{artData.title}</h2>
+    <h4>{artData.displayname}</h4>
+    <h4>{artData.worktype}</h4>
+    <h4>{artData.yearmade}</h4>
+    <h4>{artData.commentary}</h4>
+    <h4>{artData.rank}</h4>
+    <p><button onClick={() => navigate("/Artwork")}>Return</button>
+       </p>
+    </div>
+    
+
+  )
+}
+
+export default ArtCard
