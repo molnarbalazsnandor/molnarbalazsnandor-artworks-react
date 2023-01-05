0fe4eaa0b1635da38afb9045fcedb8211ff41573
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2023-01-05 21:52:01 +0100

    favorites works!

--- my-app/src/components/Favorites.jsx
+++ my-app/src/components/Favorites.jsx
@@ -5,23 +5,6 @@ import Favorite from './Favorite'
 
 
 function Favorites({favorites, setFavorites}) {
-/*   const [favoriteArts, setFavoriteArts] = useState([]);
-  const [favoritesCounter, setFavoritesCounter] = useState(0)
-  const apiAddress = `https://api.harvardartmuseums.org/OBJECT/${favorites[favoritesCounter]}?apikey=9fcbde6d-b1de-4546-8974-eef81e8f90f4`
-
-
-  const fetchFavoriteArts = () => {
-      fetch(apiAddress)
-        .then((res) => res.json())
-        .then((data) => {
-            setFavoriteArts([...favoriteArts,data]);
-          });
-          if(favoritesCounter<favorites.length){setFavoritesCounter(favoritesCounter+1)}
-          console.log(favorites.length, favoritesCounter)
-  };
-  useEffect(() => fetchFavoriteArts(), [favoritesCounter]);
-  useEffect(() => console.log(favoriteArts), [favoriteArts]); */
-
 
   return (
     <Layout>
