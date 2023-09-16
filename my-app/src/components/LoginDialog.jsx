a64da0426f575bafac71e91d43d5929f74473764
molnarbalazsnandor <molnarbalazsnandor@gmail.com>
2023-09-16 21:49:03 +0200

    LoginDialog button upgraded

--- my-app/src/components/LoginDialog.jsx
+++ my-app/src/components/LoginDialog.jsx
@@ -1,45 +1,80 @@
 import React, { useState, useEffect } from "react";
-import { Button, Dialog, DialogContent, DialogTitle, DialogContentText, TextField, DialogActions,CircularProgress } from '@mui/material/'
-import {Link} from "react-router-dom"
-import {GoogleButton} from "react-google-button"
-import {UserAuth} from "../context/AuthContext"
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogContentText,
+  TextField,
+  DialogActions,
+  CircularProgress,
+} from "@mui/material/";
+import { Link } from "react-router-dom";
+import { GoogleButton } from "react-google-button";
+import { UserAuth } from "../context/AuthContext";
 
-function LoginDialog({openLogin,setOpenLogin}) {
-  
-  const {googleSignIn, logOut, user} = UserAuth();
-  
+function LoginDialog({ openLogin, setOpenLogin }) {
+  const { googleSignIn, logOut, user } = UserAuth();
 
   const handleGoogleSignIn = async () => {
-    try{
-      await googleSignIn()
-    } catch (error){
-      console.log(error)
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   const handleSignOut = async () => {
-    try{
-      await logOut()
-    } catch (error){
-      console.log(error)
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
   return (
     <div>
       <Dialog open={openLogin}>
         <DialogTitle>Login</DialogTitle>
         <DialogContent>
-            <DialogContentText>
-              You need to log in to mark artworks as favorites!
-            </DialogContentText>
+          <DialogContentText>
+            You need to log in to mark artworks as favorites!
+          </DialogContentText>
         </DialogContent>
-        <DialogActions>
-          <GoogleButton onClick={()=> {handleGoogleSignIn(); setOpenLogin(false)}}>Sign in with Google</GoogleButton> 
-          <Button onClick={() => {setOpenLogin(false)}}>OK</Button> 
+        <DialogActions
+          sx={{
+            width: "90%",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <GoogleButton
+            onClick={() => {
+              handleGoogleSignIn();
+              setOpenLogin(false);
+            }}
+          >
+            Sign in with Google
+          </GoogleButton>
+          <Button
+            sx={{
+              background: "none",
+              textTransform: "none",
+              color: "black",
+              fontSize: "16px",
+              height: "5vh",
+              fontFamily: "Oxygen, sans-serif",
+            }}
+            className="user-button"
+            onClick={() => {
+              setOpenLogin(false);
+            }}
+          >
+            OK
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
-  )
+  );
 }
 
-export default LoginDialog
+export default LoginDialog;
